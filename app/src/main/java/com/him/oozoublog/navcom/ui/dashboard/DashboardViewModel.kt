16579fe1c28ec6229ee30fac9ba2631802bd6a5a
Abdulrohim 'Him' Sama <a.rohim.sama@gmail.com>
2020-10-06 11:50:22 +0700

    first commit

---
+++ app/src/main/java/com/him/oozoublog/navcom/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.him.oozoublog.navcom.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
