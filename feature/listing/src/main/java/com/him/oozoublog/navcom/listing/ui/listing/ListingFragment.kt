a30909c355e52fce8524b5c60b8bb6eec411a106
abdulrohim <a.rohim.sama@gmail.com>
2020-10-18 20:57:10 +0700

    Added enum arguments into listing feature

--- feature/listing/src/main/java/com/him/oozoublog/navcom/listing/ui/listing/ListingFragment.kt
+++ feature/listing/src/main/java/com/him/oozoublog/navcom/listing/ui/listing/ListingFragment.kt
@@ -1,31 +1,39 @@
 package com.him.oozoublog.navcom.listing.ui.listing
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.navigation.fragment.navArgs
 import com.him.oozoublog.navcom.listing.R
 
 class ListingFragment : Fragment() {
 
     private lateinit var listingViewModel: ListingViewModel
 
+    private val args: ListingFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("search type", args.searchType.name)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        listingViewModel =
-            ViewModelProvider(this).get(ListingViewModel::class.java)
+        listingViewModel = ViewModelProvider(this).get(ListingViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_listing, container, false)
         val textView: TextView = root.findViewById(R.id.text_listing)
-        listingViewModel.text.observe(viewLifecycleOwner, {
+        listingViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        })
+        }
         return root
     }
 }
---
+++ feature/listing/src/main/java/com/him/oozoublog/navcom/listing/ui/listing/SearchType.kt
@@ -0,0 +1,7 @@
+package com.him.oozoublog.navcom.listing.ui.listing
+
+enum class SearchType {
+    ALL,
+    CAR,
+    REAL_ESTATE
+}
